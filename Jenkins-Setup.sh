ff5d618028eae9c68cf6ee8c52f1ba684cb778ab
Dev-Anas-10 <anasashraf1170@gmail.com>
2024-08-21 13:04:36 +0300

    last update

---
+++ Jenkins-Setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Install JDK 11
+sudo apt install openjdk-11-jdk -y
+
+# Download Jenkins key and store it in keyring
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# Add Jenkins repository to the sources list
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package list again
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install jenkins -y
+
+# User & password
+# user : adminanas
+# password : admin
