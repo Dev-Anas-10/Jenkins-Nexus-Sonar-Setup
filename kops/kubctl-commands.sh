a9994dc8f6b4ea044dc2247e47b14505ab0acc84
Anas <anasashraf1170@gmail.com>
2024-12-03 17:24:31 +0200

    .

--- kops/kubctl-commands.sh
+++ kops/kubctl-commands.sh
@@ -89,3 +89,5 @@ kubectl delete -f <directory-path>
 https://github.com/kubernetes/ingress-nginx/blob/main/docs/deploy/index.md
 sed -i 's|proxy-real-ip-cidr:.*|proxy-real-ip-cidr: ${{ env.CIDR }}|' deploy.yaml
 sed -i 's|arn:aws:acm:us-west-2:.*|${{ env.CERT_ARN }}|' deploy.yaml
+
+# ingress ELB in Local VM search metallb
